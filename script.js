2b8d6af55a805622bf0b70d9fdac400a539aa823
kojiso17 <46018950+kojiso17@users.noreply.github.com>
2022-03-22 16:39:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
+const userNameIs = document.querySelector(".firstPh h2");
+
+console.log(userNameIs);
+
+
+
 function checkUserName() {
   const userName = prompt('what is your name?');
+
   if (userName == "") {
-    const userName = prompt('바르게 적어주세요');
+    const userName = prompt('바르게 적어주세요.');
   } else {
+    console.log(userName);
+    userNameIs.innerText = userName;
     alert("thank you");
   }
-
 }
